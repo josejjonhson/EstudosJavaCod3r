14486a57548e66cc6a8eb59cf82be8d31ca420ce
josejjonhson <josejjonhson@gmail.com>
2024-07-18 01:32:18 -0300

    Desafio Conversão

--- fundamentos/DesafioConversao.java
+++ fundamentos/DesafioConversao.java
@@ -1,10 +1,31 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class DesafioConversao {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Olá, Mundo!");
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("Informe o primeiro salário:");
+		String valor1 = sc.next().replace(",", ".");
+		
+		System.out.print("Informe o segundo salário:");
+		String valor2 = sc.next().replace(",", ".");
+		
+		System.out.print("Informe o terceiro salário:");
+		String valor3 = sc.next().replace(",", ".");
+		
+		// Convertendo as Strings em Double
+		double salario1 = Double.parseDouble(valor1);
+		double salario2 = Double.parseDouble(valor2);
+		double salario3 = Double.parseDouble(valor3);
+		
+		double media = (salario1 + salario2 + salario3) / 3;
+		
+		System.out.println("A média dos salários é " + media);
+		
+		sc.close();
 	}
 }
