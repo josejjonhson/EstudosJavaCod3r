196750ecef4180efa892cf79780e7cf9a9ad6273
josejjonhson <josejjonhson@gmail.com>
2024-07-21 13:21:57 -0300

    Operadores Relacionais

---
+++ fundamentos/operadores/OperadoresRelacionais.java
@@ -0,0 +1,22 @@
+package fundamentos.operadores;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		
+		// Comportamento estranho que acontece no Java
+		int a = 97;
+		int b = 'a';
+		System.out.println(a == b);
+		System.out.println('\u0061');
+		
+		/**
+		 * Operadores:
+		 * >
+		 * <
+		 * >=
+		 * <= 
+		 * !=
+		 */
+	}
+}
