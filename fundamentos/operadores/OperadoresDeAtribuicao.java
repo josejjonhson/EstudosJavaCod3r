8e7c891109e9aabb34213db0527eba27e0a7d19e
josejjonhson <josejjonhson@gmail.com>
2024-07-21 13:25:18 -0300

    Operadores de Atribuição

---
+++ fundamentos/operadores/OperadoresDeAtribuicao.java
@@ -0,0 +1,24 @@
+package fundamentos.operadores;
+
+public class OperadoresDeAtribuicao {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		int d = 0;
+		d += b;
+		
+		/**
+		 * Formas reduzidas:
+		 * +=
+		 * -+
+		 * /=
+		 * *=
+		 * %=
+		 */
+		
+		
+	}
+}
