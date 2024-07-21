dc01bd3d436a7728d9e6175d0f32845d62b32e13
josejjonhson <josejjonhson@gmail.com>
2024-07-21 13:29:02 -0300

    Operadores Unários

---
+++ fundamentos/operadores/OperadoresUnarios.java
@@ -0,0 +1,18 @@
+package fundamentos.operadores;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		// Forma prefix
+		a++; // a = a + 1
+		b--; // a = a - 1
+		
+		// Forma postfix
+		++a;
+		--a;
+	}
+}
