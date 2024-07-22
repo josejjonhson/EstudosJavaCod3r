62f0e78f8b0d1a6f3f03a119602f384e26bbe549
josejjonhson <josejjonhson@gmail.com>
2024-07-22 15:07:33 -0300

    Utilizando o for

---
+++ fundamentos/controle/For1.java
@@ -0,0 +1,11 @@
+package fundamentos.controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		
+		for(int c = 0; c < 10; c++) {
+			System.out.println("Olá, Mundo!");
+		}
+	}
+}
