13b488683f2218f53fb097a483707fe6521639c4
josejjonhson <josejjonhson@gmail.com>
2024-07-22 18:06:51 -0300

    Create Break.java

---
+++ fundamentos/controle/Break.java
@@ -0,0 +1,17 @@
+package fundamentos.controle;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if (i == 5) {
+				System.out.println();
+				break;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Fim!");
+	}
+}
