c07b53974ed2b721bc0adb1597c5e01d59e8f060
josejjonhson <josejjonhson@gmail.com>
2024-07-22 15:17:39 -0300

    Create DesafioWhileIndeterminado.java

---
+++ fundamentos/controle/DesafioWhileIndeterminado.java
@@ -0,0 +1,22 @@
+package fundamentos.controle;
+
+import java.util.Scanner;
+
+public class DesafioWhileIndeterminado {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String repete = null;
+		while(!"parar".equals(repete)) {
+			System.out.println("Entrando no loop novamente!");
+			System.out.print("Digite alguma coisa >> ");
+			repete = in.nextLine();
+		}
+		
+		System.out.println("Saiu do loop!");
+		
+		in.close();
+	}
+}
