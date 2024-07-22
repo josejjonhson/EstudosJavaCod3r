f4dc23d449c4512a8b61b7099df2c478362da91a
josejjonhson <josejjonhson@gmail.com>
2024-07-22 19:28:19 -0300

    Utilizando o break

---
+++ fundamentos/controle/Break2.java
@@ -0,0 +1,23 @@
+package fundamentos.controle;
+
+public class Break2 {
+
+	public static void main(String[] args) {
+
+		// Ter cuidado com a utilização que vai ser mostrada aqui.
+		// Devemos evitar!!!
+
+		externo: for (int i = 0; i < 3; i++) { // Rotulamos o for externo e queremos que o break atue nele.
+			interno: for (int j = 0; j < 3; j++) {
+
+				if (i == 1) {
+					break externo;
+				}
+
+				System.out.printf("[%d %d]", i, j);
+			}
+			System.out.println();
+		}
+		System.out.println("FIM!");
+	}
+}
