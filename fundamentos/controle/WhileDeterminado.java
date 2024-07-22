4b0b8673aca5ea8ef1838f6e567092be67b323c2
josejjonhson <josejjonhson@gmail.com>
2024-07-22 15:06:12 -0300

    Usando o while

---
+++ fundamentos/controle/WhileDeterminado.java
@@ -0,0 +1,13 @@
+package fundamentos.controle;
+
+public class WhileDeterminado {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		while(contador < 10) {
+			System.out.println("Olá, Mundo!");
+			contador++;
+		}
+	}
+}
