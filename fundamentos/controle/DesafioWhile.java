f67528be616d7cd0375c3234dc963e9da940e38b
josejjonhson <josejjonhson@gmail.com>
2024-07-22 16:29:48 -0300

    Calculadora de média

---
+++ fundamentos/controle/DesafioWhile.java
@@ -0,0 +1,33 @@
+package fundamentos.controle;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+
+		int totalDeNotas = 0;
+		double notasSomadas = 0.0;
+		boolean continua = true;
+		
+		while (continua) {
+			System.out.print("Informe uma nota >> ");
+			double nota = in.nextDouble();
+			if(nota <= 10 && nota >= 0) {
+				notasSomadas += nota;
+				totalDeNotas++;
+			} else if (nota == -1) {
+				continua = false;
+			} else {
+				System.out.println("Nota inválida!");
+				System.out.println("Só são permitidos valores de 0 a 10");
+			}
+		}
+		
+		System.out.println("A média é : " + notasSomadas / totalDeNotas);
+
+		in.close();
+	}
+}
