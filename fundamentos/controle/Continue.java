b7f44230e6915419111236b14d1a123bac8612cf
josejjonhson <josejjonhson@gmail.com>
2024-07-22 19:19:48 -0300

    Create Continue.java

---
+++ fundamentos/controle/Continue.java
@@ -0,0 +1,28 @@
+package fundamentos.controle;
+
+public class Continue {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			if (i % 2  == 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		// Quero exibir todos os valores menos o elemento 5
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+		// Outra notação
+		for (int i = 0; i < 10; i++) {
+			if (i == 5) continue;
+			System.out.println(i);
+		}
+	}
+}
