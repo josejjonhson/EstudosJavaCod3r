da376275e39a83c345f64266a3cfc6918531c6a8
josejjonhson <josejjonhson@gmail.com>
2024-07-24 14:19:27 -0300

    Data e DataTeste

---
+++ classe/DataTeste.java
@@ -0,0 +1,17 @@
+package classe;
+
+public class DataTeste {
+
+	public static void main(String[] args) {
+		
+		Data d1 = new Data();
+		d1.dia = 12;
+		d1.mes = 12;
+		d1.ano = 2003;
+		
+		Data d2 = new Data();
+		d2.dia = 01;
+		d2.mes = 01;
+		d2.ano = 2001;
+	}
+}
