e4b85dabc5fa06c9e3436ed7b98cec3f2eecb5e3
josejjonhson <josejjonhson@gmail.com>
2024-08-18 17:27:00 -0300

    oo

---
+++ oo/heranca/Jogo.java
@@ -0,0 +1,13 @@
+package oo.heranca;
+
+public class Jogo {
+
+	public static void main(String[] args) {
+		
+		Jogador j1 = new Jogador();
+		j1.andar(Direcao.NORTE);	
+		j1.andar(Direcao.NORTE);	
+		j1.andar(Direcao.NORTE);	
+		System.out.println(j1.y);
+	}
+}
